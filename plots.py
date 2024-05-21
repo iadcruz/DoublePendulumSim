19881318fe2ecc64c7d4df8f3401f332c49cf58e
iadcruz <87490500+iadcruz@users.noreply.github.com>
2024-05-21 11:27:56 -0400

    asdf

--- plots.py
+++ plots.py
@@ -22,7 +22,7 @@ y1 = -l1 * np.cos(t1)
 x2 = x1 + l2 * np.sin(t2)
 y2 = y1 - l2 * np.cos(t2)
 
-fig, ax = plt.subplots(2, 2, layout="constrained")
+fig, ax = plt.subplots(1, 2, layout="constrained")
 ax[0].set_xlim(-2, 2)
 ax[0].set_ylim(-2, 2)
 
@@ -30,18 +30,18 @@ line, = ax[0].plot([], [], 'o-', lw=2)
 trace, = ax[0].plot([], [], 'b-', lw=1)
 ax[0].set_title('Double Pendulum')
 
-def initDouble():
+def init():
     line.set_data([], [])
     trace.set_data([], [])
     return line, trace
 
-def updateDouble(i):
+def update(i):
     thisx = [0, x1[i], x2[i]]
     thisy = [0, y1[i], y2[i]]
     line.set_data(thisx, thisy)
     trace.set_data(x2[:i], y2[:i])
     return line, trace
 
-ani = animation.FuncAnimation(fig, updateDouble, frames=len(t), init_func=initDouble, blit=True, interval=dt*1000)
+ani = animation.FuncAnimation(fig, update, frames=len(t), init_func=init, blit=True, interval=dt*1000)
 
 plt.show()
